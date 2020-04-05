75db0dc2a72ce50de9b7faf7a81ce43c749dee8d
Naoki Mita <naoki.mita18@gmail.com>
2020-04-05 14:29:15 -0700

    Set PizzaRestaurant starter package for this exercise

---
+++ Lab8/Lab8/Kitchen.m
@@ -0,0 +1,18 @@
+//
+//  Kitchen.m
+//  PizzaRestaurant
+//
+//  Created by Steven Masuch on 2014-07-19.
+//  Copyright (c) 2014 Lighthouse Labs. All rights reserved.
+//
+
+#import "Kitchen.h"
+
+@implementation Kitchen
+
+/*- (Pizza *)makePizzaWithSize:(PizzaSize)size toppings:(NSArray *)toppings
+{
+    
+}*/
+
+@end
