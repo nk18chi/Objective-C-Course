65ebf96a6120bb008c63f35a97715bad5523bf9a
Naoki Mita <naoki.mita18@gmail.com>
2020-04-07 20:51:56 -0700

    Create project for Lab6

---
+++ Lab6/Lab6/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Lab6
+//
+//  Created by 美田直毅 on 2020-04-07.
+//  Copyright © 2020 Naoki Mita. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
